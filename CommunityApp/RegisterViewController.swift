dd866582c4ad1172490bc87a62a4c4617e81f575
Dan Esrey <danesrey@dans-air.lan>
2016-10-10 16:55:57 -0400

    Built Register UI

---
+++ CommunityApp/RegisterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.swift
+//  CommunityApp
+//
+//  Created by Dan Esrey on 2016/10/10.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+}
